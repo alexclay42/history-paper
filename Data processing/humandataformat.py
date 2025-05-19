fece8e03c1745a560ba76bda0c8acd1b5edd48e7
codesubmissionanon112 <toot-bosses.0k@icloud.com>
2025-05-19 08:00:15 +0100

    adding folder

---
+++ Data processing/humandataformat.py
@@ -0,0 +1,62 @@
+#https://github.com/sstepput/Avalon-NLU/tree/main
+
+ #Avalon-NLU-main/dataset
+import json
+import os
+import pandas as pd
+
+def read_json(path):
+    with open(path, mode="r", encoding="utf-8") as f:
+        json_data = json.load(f)
+        f.close()
+    return json_data
+
+# python humandataformat.py 
+# git clone https://github.com/sstepput/Avalon-NLU.git
+#messages, beliefs , persuasion need to match mid with messages and ersuasion
+#91 accuracy for unidecor, 755 accuraccy for mafiascum only, should change train test amounts 
+reformattedDataset = []
+fileList  = os.listdir("Avalon-NLU/dataset/") 
+print(fileList )
+print(len(fileList) )
+
+#print(fileInfo["messages"])
+def matchMids(msgs, persuasion, file):
+    for m in msgs:
+        msg = msgs.get(m)
+        
+        
+        for d in persuasion:
+            dec = persuasion.get(d)
+
+            
+
+            if msg.get("mid") == dec.get("mid"):
+                msg['file'] = file
+                if dec.get("deception") != None:
+                    print(dec)
+                    dec['label'] = "D"
+                else: 
+                    dec['label'] = "T"
+                    dec.update({'deception': 'none'})
+
+
+#                print(msgs.get(m) )
+                #print(m.get("mid"))
+#                print(dec )
+                reformattedDataset.append(dict(dec,**msg))
+
+
+for file in fileList :
+    fileInfo = read_json("Avalon-NLU/dataset/"+ file)
+    msgs = fileInfo["messages"]
+    persuasion = fileInfo["persuasion"]
+
+    matchMids(msgs, persuasion, file)
+
+print(len(reformattedDataset))
+print(reformattedDataset)
+df = pd.DataFrame(reformattedDataset)
+print(df)
+df.to_csv('humanLogs.csv', sep='\t')
+#df.to_json('humanLogs.json')
---
+++ readme.md
@@ -0,0 +1,10 @@
+Repository for the paper "Looking Into The Past: The Impact of Context on Detecting Deceptive Statements"
+
+
+Mafia data available: https://github.com/omonida/mafia-dataset
+Avalon data available: https://github.com/sstepput/Avalon-NLU/tree/main/dataset
+
+The models were too large to include, but can easily be re-run from the scripts provided.
+The evaluations were initially run in the eval files, but were re-done with manual equations for confirmation. 
+
+
